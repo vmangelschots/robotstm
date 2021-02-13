05bcec6fb6674a6e3aedc63c440f4f4bd726b243
Mangelschots Vincent <vincent@mangelschots.org>
2021-02-13 09:29:57 +0100

    verder gewerkt

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,9 @@ int pos[7];
 void UpdateServoPos(void *pvParameters){
 
   for(;;){
+    //Serial.println("updateservopos");
     for(int i = 0;i<7;i++){
+
       channels[i].write(map(pos[i],0,255,0,180));
     }
     vTaskDelay(10/portTICK_PERIOD_MS);
@@ -36,6 +38,13 @@ void TaskStatus(void *pvParameters)  // This is a task.
   pinMode(PA_8,OUTPUT);
   for (;;) // A Task shall never return or exit.
   {
+    for(int i = 0;i<7;i++){
+      Serial.print("channel ");
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.println(pos[i]);
+      
+    }
     digitalWrite(PA_8, HIGH);   // turn the LED on (HIGH is the voltage level)
     vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
     digitalWrite(PA_8, LOW);    // turn the LED off by making the voltage LOW
@@ -43,22 +52,36 @@ void TaskStatus(void *pvParameters)  // This is a task.
   }
 }
 
+void GetFromI2c(void *pvParameters)  // This is a task.
+{
+  (void) pvParameters;
+  char command;
+  for(;;){
+    Serial.println("i2cloop");
+    if(Wire.available()){
+        Serial.println("whoehoew. I got some data");
+        command = Wire.read();
+        if(command=='S'){
+          for(int i = 0; i< 7; i++){
+            pos[i] = Wire.read();
+          }
+        }
+        else{
+          command = Wire.read();
+        }
+      }
+      vTaskDelay(100/portTICK_PERIOD_MS);
+  }
+  
+}
+
 void receiveEvent(int howMany)
 {
-  char command;
-  Serial.print("received");
-  while(Wire.available()){
-    command = Wire.read();
-    if(command=='S'){
-      for(int i = 0; i< 7; i++){
-        pos[i] = Wire.read();
-      }
-    }
-    else{
-      command = Wire.read();
-    }
-  }
+  
+    Serial.println("whoehoew. I got some data");
 }
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -73,6 +96,12 @@ void setup() {
     Serial.println(index);
 
   }
+
+  Wire.setSCL(PB_6);
+  Wire.setSDA(PB_7);
+  Wire.begin(0x03);
+  //Wire.onReceive(receiveEvent);
+
   Serial.println("setting up task");
   xTaskCreate(
     TaskStatus,
@@ -99,19 +128,16 @@ void setup() {
   NULL,
   5,  // priority
   NULL );
-  //   xTaskCreate(
-  // receiveI2C,
-  // "receiveI2C",   // A name just for humans
-  // 1024,  // Stack size
-  // NULL,
-  // 3,  // priority
-  // NULL );
+     xTaskCreate(
+   GetFromI2c,
+   "GetFromI2c",   // A name just for humans
+   1024,  // Stack size
+   NULL,
+   3,  // priority
+   NULL );
   Serial.println("starting...");
 
-  Wire.begin(3);
-  Wire.setSCL(PB_6);
-  Wire.setSDA(PB_7);
-  Wire.onReceive(receiveEvent);
+
 
   vTaskStartScheduler();
 }
